f73d95a90150885ff61c2189a0d6dbc288d4eaf4
nperez <nperez@cpan.org>
2009-04-18 06:03:23 +0000

    r22@worktop:  nicholas | 2007-07-18 12:47:56 -0500  Regex.pm now compiles!

--- lib/POE/Filter/SimpleHTTP/Regex.pm
+++ lib/POE/Filter/SimpleHTTP/Regex.pm
@@ -4,65 +4,13 @@ use warnings;
 use strict;
 
 use Regexp::Common('pattern');
-use Regexp::Common::URI('http');
+use Regexp::Common::URI::http;
 
-my $oct			= gen_octet();
-my $char		= gen_char();
-my $upalpha		= '[A-Z]';
-my $loalpha		= '[a-z]';
-my $alpha		= "(?:$upalpha|$loalpha)";
-my $digit		= '[0-9]';
-my $ctrl		= gen_ctrl();
-my $cr			= chr(13);
-my $lf			= chr(10);
-my $sp			= chr(32);
-my $ht			= chr(9);
-my $dq			= chr(34);
-my $crlf		= "(?:$cr$lf)";
-my $lws			= "(?:$crlf*(?:$sp|$ht)+)";
-my $text		= '(?:' . exclude($ctrl,$oct) .' )';
-my $hex			= "[a-fA-F]|$digit]";
-my $separators 	= gen_separators();
-my $token		= '(?:' . exclude( $ctrl, exclude( $separators, $char ) ). ')';
-my $ctext		= '(?:' . exclude( "[()]", $text ) . ')';
-my $quot_pair	= "(?:\\$char)"
-my $comment		= "(\((?:$ctext|$quot_pair|\1)*\))";
-my $qdtext		= '(?:' . exclude( q/"/, $text ) . ')';
-my $quot_str   	= "(\"(?:$qdtext|$quot_par)*\")";
-
-my $httpvers	= "(?:HTTP\/$digit+\.$digit+)";
-
-my $f_content	= "(?:$text|$token|$separators|$quot_str)*";
-my $f_value		= "(?:$f_content|$lws)*";
-
-my $header		= "(?:(?k:$token):(?k:$f_value)*)";
-my $method 		= "[OPTIONS|GET|HEAD|POST|PUT|DELETE|CONNECT|$token]";
-my $req_line	= "(?:(?k:$method)$sp(?k:$RE{'URI'}->{'HTTP'})"
-				. "$sp(?k:$httpvers)$crlf)";
-my $resp_code	= "[[:digit:]]{3}";
-my $resp_line	= "(?:(?k:$httpvers)$sp(?k:$resp_code)$sp(?k:$text)*$crlf)";
-
-#export header pattern
-pattern
-(
-	'name'		=> ['PFSH', 'header'],
-	'create'	=> $header,
-);
-
-#export request pattern
-pattern
-(
-	'name'		=> ['PFSH', 'request'],
-	'create'	=> $req_line,
-);
-
-#export response pattern
-pattern
-(
-	'name'		=> ['PFSH', 'response'],
-	'create'	=> $resp_line,
-);
-
+sub quote_it
+{
+    $_[0] =~ s/([^[:alnum:][:cntrl:][:space:]])/\\$1/g;
+	return $_[0];
+}
 
 sub gen_char
 {
@@ -113,26 +61,83 @@ sub gen_octet
     return '[' . quote_it($foo) . ']';
 }
 
-sub quote_it
-{
-    $_[0] =~ s/([^[:alnum:][:cntrl:][:space:]])/\\$1/g;
-	return $_[0];
-	return "[$foo]";
-}
-
 sub gen_separator
 {
 	my $foo = join 
 	(
 		'|',
 		( 
-			map { q#\#.$_ } 
-				qw# ( ) < > @ , ; : \ " / [ ] ? = { } #
+			map { q|/|.$_ } 
+			(
+				split
+				(
+					/\s/,
+				 	'( ) < > @ , ; : \ " / [ ] ? = { }'
+				)
+			)
 		)
 	);
 
-	$foo .= "$sp | $ht";
+	$foo .= chr(32) . ' | ' . chr(9);
 
 	return "[$foo]";
 }
 
+my $oct			= gen_octet();
+my $char		= gen_char();
+my $upalpha		= '[A-Z]';
+my $loalpha		= '[a-z]';
+my $alpha		= "(?:$upalpha|$loalpha)";
+my $digit		= '[0-9]';
+my $ctrl		= gen_ctrl();
+my $cr			= chr(13);
+my $lf			= chr(10);
+my $sp			= chr(32);
+my $ht			= chr(9);
+my $dq			= chr(34);
+my $crlf		= "(?:$cr$lf)";
+my $lws			= "(?:$crlf*(?:$sp|$ht)+)";
+my $text		= '(?:' . exclude($ctrl,$oct) .' )';
+my $hex			= "[a-fA-F]|$digit]";
+my $separators 	= gen_separators();
+my $token		= '(?:' . exclude( $ctrl, exclude( $separators, $char ) ). ')';
+my $ctext		= '(?:' . exclude( "[()]", $text ) . ')';
+my $quot_pair	= "(?:\\$char)";
+my $comment		= "(\((?:$ctext|$quot_pair|\1)*\))";
+my $qdtext		= '(?:' . exclude( q/"/, $text ) . ')';
+my $quot_str   	= "(\"(?:$qdtext|$quot_pair)*\")";
+
+my $httpvers	= "(?:HTTP\/$digit+\.$digit+)";
+
+my $f_content	= "(?:$text|$token|$separators|$quot_str)*";
+my $f_value		= "(?:$f_content|$lws)*";
+
+my $header		= "(?:(?k:$token):(?k:$f_value)*)";
+my $method 		= "[OPTIONS|GET|HEAD|POST|PUT|DELETE|CONNECT|$token]";
+my $req_line	= "(?:(?k:$method)$sp(?k:$RE{'URI'}->{'HTTP'})"
+				. "$sp(?k:$httpvers)$crlf)";
+my $resp_code	= "[[:digit:]]{3}";
+my $resp_line	= "(?:(?k:$httpvers)$sp(?k:$resp_code)$sp(?k:$text)*$crlf)";
+
+#export header pattern
+pattern
+(
+	'name'		=> ['PFSH', 'header'],
+	'create'	=> $header,
+);
+
+#export request pattern
+pattern
+(
+	'name'		=> ['PFSH', 'request'],
+	'create'	=> $req_line,
+);
+
+#export response pattern
+pattern
+(
+	'name'		=> ['PFSH', 'response'],
+	'create'	=> $resp_line,
+);
+
+1;
